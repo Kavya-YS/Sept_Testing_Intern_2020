fd2c1dbb8c7b2adaa6dc6cd4c7c2879d8737aff8
Kavya-YS <kavya.suryaprakash@qxf2.com>
2020-09-11 19:31:37 +0530

    commit Square_in_reverse program

---
+++ Square_in_reverse.py
@@ -0,0 +1,17 @@
+"""Get the input number
+determine the range of the number
+While iterating through the range of number square the number
+print the squared number of each iteration
+"""
+
+#Fetch the number through user input
+num=int(input("Input number is: "))
+
+#Determine the range of "num"
+num_list=range(num)
+
+#iterate through the range of "num"
+for x+1 in num_list:
+#Find the square of each iterated num and print the value    
+    print(x*x)
+    
