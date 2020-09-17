d60118dcbde89606d2c20ac29f4e80fc19efd282
Kavya-YS <kavya.suryaprakash@qxf2.com>
2020-09-17 17:54:16 +0530

    Completed the program to click on Buy Moisturizers or Buy Sunscreens based on the temperature

--- weathershopper_choose_lotion_temperature_based.py
+++ weathershopper_choose_lotion_temperature_based.py
@@ -16,33 +16,33 @@ browser= webdriver.Chrome()
 #Navigate to the weathershopper home page
 browser.get("http://weathershopper.pythonanywhere.com/")
 
-#Locate the temperature and print
+#Locate the temperature web application and print the temperature
 temperature= browser.find_element_by_id("temperature").text
 print("temperature:", temperature)
 time.sleep(3)
+
+#Splitting the string to get a list
 dynamic_temperature= temperature.split(" ")
 print("dynamic_temperature=", dynamic_temperature)
-indices= [0]
-current_temperature = [dynamic_temperature[index] for index in indices]
-print(*current_temperature)
 
-#select Moisturizers if the current_temperature is 19 degree and below
-def checkforless(current_temperature, val1):
-    val1=19
-    for x in current_temperature:
-        if val1 >= current_temperature:
-            print("Buy Moisturizers")
-        else:
-            print("Buy Sunscreens")
+#Printing on the numerical value alone excluding the string
+print(dynamic_temperature[0])
+
+#Converting the String to an Integer
+current_temperature=int(dynamic_temperature[0])
+print(type(current_temperature))
+
+#Click on "Buy Moisturizers" button if the temperature is <=19 and click on "Buy Sunscreens" button if the temperature is >=34
+if(current_temperature)<=19:
+    buy_moisturizers= browser.find_element_by_xpath("//button[@class='btn btn-primary' and text()='Buy moisturizers']").click()
+    time.sleep(2)
+    print("Buy Moisturizer")
+elif(current_temperature>=34):
+    buy_sunscreens= browser.find_element_by_xpath("//button[@class='btn btn-primary' and text()='Buy sunscreens']").click()
+    time.sleep(2)
+    print("Buy Sunscreen")
+else:
+    print("No lotion required")
+time.sleep(2)    
 browser.quit()
-"""if (CheckForLess(current_temperature, val1)): print("Yes") 
-else: print("No") 
-
-
-if current_temperature<=19:
-    print("Buy Moisturizers")
-    #buy_moisturizers=browser.find_element_by_xpath("//button[@class='btn btn-primary']").click()
-else: #current_temperature>=34:
-    print("Buy Sunscreens")"""
-
 
