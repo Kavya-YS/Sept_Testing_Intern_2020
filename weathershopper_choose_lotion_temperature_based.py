5c05ecea185b74cea3a5b97cac7faead4d7687c4
Kavya-YS <kavya.suryaprakash@qxf2.com>
2020-09-18 11:21:39 +0530

    Made the changes as per code review for the print statements and confirmation message for passing test

--- weathershopper_choose_lotion_temperature_based.py
+++ weathershopper_choose_lotion_temperature_based.py
@@ -16,31 +16,37 @@ browser= webdriver.Chrome()
 #Navigate to the weathershopper home page
 browser.get("http://weathershopper.pythonanywhere.com/")
 
+#Get the title of the page
+if(browser.title=="Current Temperature"):
+    print("Tittle of the home page is 'Current Temperature'")
+else: 
+    print("Unable to get title")
+
 #Locate the temperature web application and print the temperature
 temperature= browser.find_element_by_id("temperature").text
-print("temperature:", temperature)
 time.sleep(3)
 
 #Splitting the string to get a list
 dynamic_temperature= temperature.split(" ")
-print("dynamic_temperature=", dynamic_temperature)
-
-#Printing on the numerical value alone excluding the string
-print(dynamic_temperature[0])
 
 #Converting the String to an Integer
 current_temperature=int(dynamic_temperature[0])
-print(type(current_temperature))
 
 #Click on "Buy Moisturizers" button if the temperature is <=19 and click on "Buy Sunscreens" button if the temperature is >=34
 if(current_temperature)<=19:
     buy_moisturizers= browser.find_element_by_xpath("//button[@class='btn btn-primary' and text()='Buy moisturizers']").click()
     time.sleep(2)
-    print("Buy Moisturizer")
+    if(browser.title=="The Best Moisturizers in the World!"):
+        print("The Best Moisturizers in the World!")
+    else: 
+        print("Unable to get Moisturizer page title")
 elif(current_temperature>=34):
     buy_sunscreens= browser.find_element_by_xpath("//button[@class='btn btn-primary' and text()='Buy sunscreens']").click()
     time.sleep(2)
-    print("Buy Sunscreen")
+    if(browser.title=="The Best Sunscreens in the World!"):
+        print("The Best Sunscreens in the World!")
+    else: 
+        print("Unable to get title")
 else:
     print("No lotion required")
 time.sleep(2)    
