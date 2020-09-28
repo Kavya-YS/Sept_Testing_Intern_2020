b314e9bd1cf17ded2c7f335dace4487cd91f30d4
Kavya-YS <kavya.suryaprakash@qxf2.com>
2020-09-28 20:54:10 +0530

    ws_minAloe_minAlmond.py

--- ws_minAloe_minAlmond.py
+++ ws_minAloe_minAlmond.py
@@ -30,10 +30,12 @@ moisturizers= browser.find_elements_by_xpath("//div[contains(@class, 'text-cente
 
 product_name_list= []
 product_price_list=[]
-sub_string_of_name= "Aloe"
+sub_string_of_aloe= "Aloe"
 aloe_essence=[]
-index_aloe_price=[]
 aloe_price=[]
+sub_string_of_almond= "Almond"
+almond_essence=[]
+almond_price=[]
 
 for each_moisturizer in moisturizers:
   #print(each_moisturizer.text)
@@ -59,18 +61,62 @@ time.sleep(3)
 
 #Get the list list of products containing Aloe essence
 for  product_name in product_name_list:
-    if sub_string_of_name in product_name:
+    if sub_string_of_aloe in product_name:
         aloe_essence.append(product_name)
 print("Aloe based moisturizers are:", aloe_essence)
 
-#Get the index of all Aloe based moisturizers from the product_name_list
+#Get the list of prices for Aloe essence moisturizers
 index_aloe_name= [product_name_list.index(item) for item in aloe_essence]
-print("index_aloe_name:", index_aloe_name)
-index_aloe_price= index_aloe_price.insert(index_aloe_name)
-print("index_aloe_price:", index_aloe_price)
-"""for index in index_aloe_name:
-  for product_price in product_price_list:"""
+#print("index_aloe_name:", index_aloe_name)
+aloe_price= [product_price_list[i] for i in index_aloe_name]
+print("aloe_price:", aloe_price)
 
+#Get minimum price of Aloe essence moisturizers
+min_price_aloe= aloe_price[0]
+for each_aloe_price in range(0, len(aloe_price), 1):
+  min_price_aloe= min(min_price_aloe,aloe_price[each_aloe_price])
+print("Minimum priced Aloe in the list:", min_price_aloe)
+
+
+#Get the name of minimum priced Almond moisturizer
+index_min_price_Aloe= aloe_price.index(min_price_aloe)
+#print("Index of the minimum priced Aloe moisturizer:", index_min_price_Aloe)
+print("Name of the minimum priced Aloe Moisturizer:", aloe_essence[index_min_price_Aloe])
+
+
+#Add the minimum priced Aloe based moisturizer to the cart by clicking on the add button
+aloe_add_to_cart= browser.find_element_by_xpath("//button[contains(@onclick, 'addToCart')]").click()
+
+#Get the list list of products containing Almond essence
+for  product_name in product_name_list:
+    if sub_string_of_almond in product_name:
+        almond_essence.append(product_name)
+print("Almond based moisturizers are:", almond_essence)
+
+#Get the list of prices for Almond essence moisturizers
+index_almond_name= [product_name_list.index(item) for item in almond_essence]
+#print("index_almond_name:", index_almond_name)
+almond_price= [product_price_list[i] for i in index_almond_name]
+print("almond_price:", almond_price)
+
+#Get minimum price of Almond essence moisturizers
+min_price_almond= almond_price[0]
+for each_almond_price in range(0, len(almond_price), 1):
+  min_price_almond= min(min_price_almond,almond_price[each_almond_price])
+print("Minimum priced Almond in the list:", min_price_almond)
+
+#Get the name of minimum priced Almond moisturizer
+index_min_price_Almond= almond_price.index(min_price_almond)
+#print("Index of the minimum priced Almond moisturizer:", index_min_price_Almond)
+print("Name of the minimum priced Almond Moisturizer:", almond_essence[index_min_price_Almond])
+
+#Add the minimum priced Almond based moisturizer to the cart byclicking on the add button
+aloe_add_to_cart= browser.find_element_by_xpath("//button[contains(@onclick, 'addToCart')]").click()
+
+
+
+#//button[contains(@onclick, 'addToCart(almond_essence[index_min_price_Almond], min_price_almond
+#"//button[@onclick='addToCart(almond_essence[index_min_price_Almond], min_price_almond)']"
   
 
 
