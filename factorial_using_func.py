f1a4affd15f7656cf345fb0cbda17f7afea3c997
Kavya-YS <kavya.suryaprakash@qxf2.com>
2020-10-05 17:51:09 +0530

    Wrote the program to calculate factorial for range of numbers using functions and saving

---
+++ factorial_using_func.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+import math
+
+def user_input():
+    "Input the number for calculating factorial"
+    num= int(input("Enter an integer:"))
+    return num
+
+def factorial_calc(num):
+    "Calculate the factorial for the range of numbers from the user input"
+    for num in range(num):
+        fact_num=math.factorial(num)
+        print(fact_num, end=', ')
+
+if __name__=="__main__":
+    num= user_input()
+    factorial_calc(num)
+
