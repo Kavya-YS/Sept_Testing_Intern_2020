ad1f28a6dfe6b0d6675ec62c2c612d98cf27cd7c
Kavya-YS <kavya.suryaprakash@qxf2.com>
2020-10-05 16:25:09 +0530

    committing the changes made to divisible_of_7.py

--- divisibles_of_7.py
+++ divisibles_of_7.py
@@ -1,13 +1,9 @@
 def divisble_7():
+    "Print numbers divisible by 7 and not divisible by 5"
     for num in range(1999, 3201):
         if num%7==0 and num%5!=0:
             print(num, end=',')
             
-
-
-
-
-
 
 if __name__=="__main__":
     divisble_7()
